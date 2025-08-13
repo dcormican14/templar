b52f5e809a5122924977fd9b7ead027a913039d2
dcormican14 <dcormican14@gmail.com>
2025-08-13 05:39:40 -0500

    fixed small bugs

--- templar/src/app/components/atoms/index.ts
+++ templar/src/app/components/atoms/index.ts
@@ -3,37 +3,9 @@ export { Button } from './Button';
 export type { ButtonProps } from './Button';
 export { 
   Icon,
-  IconArrowRight,
-  IconArrowLeft,
-  IconCheck,
-  IconX,
-  IconBell,
-  IconUser,
-  IconSettings,
-  IconSearch,
-  IconPlus,
-  IconMinus,
-  IconEdit,
-  IconDelete,
-  IconEye,
-  IconEyeOff,
-  IconHeart,
-  IconStar,
-  IconHome,
-  IconMenu,
-  IconWarning,
-  IconInfo,
-  IconLoading,
-  IconLock,
-  IconUnlock,
-  IconDownload,
-  IconUpload,
-  IconShare,
-  IconCopy,
-  IconSun,
-  IconMoon
+  type IconProps, 
+  type IconName 
 } from './Icon';
-export type { IconProps, IconName } from './Icon';
 
 // Re-export all atomic components
 export * from './Button';
